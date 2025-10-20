28902ea3a5ce7fdac8c15d07e242b2c7ff3f3b3f
JonasMahlburg <darrylmanga@yahoo.com>
2025-10-20 12:21:10 +0200

    feat: game is now 8 questions long and Score is tracked refactor: optimized UI

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -12,49 +12,93 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
+    @State private var tryAgain = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var guesses = 0
     
     var body: some View {
         ZStack{
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
-            
-            VStack(spacing: 30){
-                VStack{
-                    Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                        .font(.subheadline.weight(.heavy))
+            VStack{
+                Spacer()
+                Text("Guess the Flag!")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 15){
+                    VStack{
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
                     
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
-                
-                ForEach(0..<3){ number in
-                    Button{
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .clipShape(.rect)
-                            .shadow(radius: 5)
+                    ForEach(0..<3){ number in
+                        Button{
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.rect)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Text("Guesses: \(guesses) of 8")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
         }message: {
-            Text("Your Score is ???")
+            Text("Your Score is \(score)")
+        }
+        .alert("Congrats!", isPresented: $tryAgain){
+            Button("Try Again", action: {
+                score = 0
+                guesses = 0
+                askQuestion()
+            })
+        }message: {
+            Text("You earned \(score) points")
         }
     }
     
     func flagTapped(_ number: Int){
         if number == correctAnswer{
             scoreTitle = "Correct"
+            score += 1
+            guesses += 1
         }else {
-                 scoreTitle = "Wrong"
+                 scoreTitle = "Wrong you picked: \(countries[number])"
+                 guesses += 1
         }
-        showingScore = true
+        if guesses == 8{
+            tryAgain = true
+        }else{
+            showingScore = true
+        }
+        
     }
     
     func askQuestion(){
